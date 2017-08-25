7d1b2999a90c96aa448bdec302c228eceeec5aac
sam <1002064@gmx.com>
2017-08-25 18:54:44 +0100

    added devise views and changed them to haml

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   resources :categories
   resources :products
   root to: "categories#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_scope :user do
+     get '/users/sign_out' => 'devise/sessions#destroy'
+   end
 end
